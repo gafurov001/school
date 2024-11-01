ef3f06daaf166a1d2090aba844e0e11e403a7062
gafurov001 <fakhriddingafurv@gmail.com>
2024-11-01 12:34:50 -0700

    first commit

--- root/urls.py
+++ root/urls.py
@@ -20,7 +20,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('apps.urls')),
+    path('', include('apps.urls')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
