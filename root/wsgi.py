90f219ffe38c80abe5994840b4a6c93340277fd5
gafurov001 <fakhriddingafurv@gmail.com>
2024-11-01 13:18:36 -0700

    first commit

--- root/wsgi.py
+++ root/wsgi.py
@@ -1,7 +1,11 @@
 import os
+import django
+
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'root.settings')
 
 application = get_wsgi_application()
+
+django.setup()
