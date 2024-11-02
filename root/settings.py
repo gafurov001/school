d966d30dbd4deb6a4bfd88a72589e8a667bde780
gafurov001 <fakhriddingafurv@gmail.com>
2024-11-02 05:37:41 -0700

    first commit

--- root/settings.py
+++ root/settings.py
@@ -18,11 +18,13 @@ INSTALLED_APPS = [
     'apps.apps.AppsConfig',
 
     'drf_spectacular',
-
+    'corsheaders',
 
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -30,6 +32,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'root.urls'
@@ -104,3 +107,10 @@ SPECTACULAR_SETTINGS = {
     'SERVE_INCLUDE_SCHEMA': False,
     # OTHER SETTINGS
 }
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:7070",  # Frontend domeni
+]
+
+CORS_ALLOW_ALL_ORIGINS = True
+
